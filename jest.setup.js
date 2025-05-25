1c2734ec553e06d564beeea7789e4c8b7b7fb398
meug <meug.ferreira@gmail.com>
2025-05-25 10:32:59 -0300

    Configuração inicial do projeto

---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom'
+
+/* Optional: Handling Absolute Imports and Module Path Aliases
+If your project is using Module Path Aliases, you will need to configure Jest to resolve the imports by matching the paths option in the jsconfig.json file with the moduleNameMapper option in the jest.config.js */
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 };
 
 export default config;
+
+
+// configuration is exported as a default export so it can be imported elsewhere
+// ES lint usually requires a default export for configuration files or it will throw an error
+// It is not recommended to disable this rule globally since it helps to identify unclear exports

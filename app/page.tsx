eb85b469604a972c3b4b794b812d2a1139359089
meug <meug.ferreira@gmail.com>
2025-05-25 17:48:59 -0300

    Aplicação funcional

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,96 @@
-import Image from "next/image";
+'use client';
+import { useState } from 'react';
+
+type Joke = {
+	id: string;
+	icon_url: string;
+	url: string;
+	value: string;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	const [userQuery, setUserQuery] = useState('');
+	const [queryResults, setQueryResults] = useState<Joke[]>([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(''); // initialize error state as empty string
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setLoading(true);
+		setError(''); // reset error state before fetching
+
+  // Get which button was used to submit
+  const submitter = (e.nativeEvent as SubmitEvent).submitter as HTMLButtonElement | null;
+  const queryInput = submitter?.value || '';
+
+		try {
+			if (queryInput === 'random') {
+				const response = await fetch('https://api.chucknorris.io/jokes/random');
+
+				if (!response.ok) throw new Error('Erro ao buscar piada aleatória. Tente novamente');
+				
+        const joke = await response.json();
+				setQueryResults([joke]); // diretamente define o resultado
+
+			} else if (queryInput === 'search') {
+				const response = await fetch(`https://api.chucknorris.io/jokes/search?query=${userQuery}`);
+
+				if (!response.ok) throw new Error('Ops! Algo deu errado com a sua pesquisa. Por favor, tente novamente.');
+
+				const data = await response.json();
+
+				if (data.result.length === 0) {
+					setError('Nenhum resultado encontrado');
+				}
+
+				setQueryResults(data.result);
+			}
+		} catch (err) {
+
+			if (err instanceof Error) { // err is an instance of Error to ensure we can access message
+				setError(err.message); // err instanceof Error ensures that err is actually an Error object so we can access properties like err.message
+
+			} else {
+				setError('Ops! Algo deu errado. Por favor, tente novamente.');
+			}
+      
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return (
+		<main className='max-w-2xl mx-auto px-4 py-10'>
+			<h1 className='text-3xl font-bold text-center mb-6'>Buscador de Pidas do Chuck</h1>
+
+			<form onSubmit={handleSearch} className='flex gap-2 mb-6'>
+				<input type='text' value={userQuery} onChange={(e) => setUserQuery(e.target.value)} placeholder='Busque uma piada' className='flex-1 p-2 border rounded' />
+				<button type='submit' name='action' value='search' className='bg-blue-600 text-white px-4 py-2 rounded'>
+					Search
+				</button>
+
+				<button type='submit' name='action' value='random' className='bg-green-600 text-white px-4 py-2 rounded'>
+					Quero uma piadoca agora!
+				</button>
+			</form>
+
+			{loading && <p className='text-center'>Carregando</p>}
+			{error && <p className='text-center text-red-600'>{error}</p>}
+
+			<ul className='space-y-4'>
+				{queryResults.map(
+					(joke) => (
+						(
+							<li key={joke.id} className='p-4 border rounded shadow'>
+								<p>{joke.value}</p>
+								{/*  <a href={joke.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 text-sm">
+              Ver na api
+            </a> */}
+							</li>
+						)
+					)
+				)}
+			</ul>
+		</main>
+	);
 }
---
+++ types/types.ts
@@ -0,0 +1,7 @@
+
+export type JokeType = {
+    id: string;
+    icon_url: string;
+    url: string;
+    value: string;
+};
